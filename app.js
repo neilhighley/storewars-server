8685de481a3e3bbb027ca9aa499632009ba64e56
vlad <vldmrs@yahoo.com>
2015-11-07 20:59:44 +0000

    built up twilio api - see twilioapi.js file in the libraries

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var twilio = require('./libraries/TwilioApi.js');
 var app = express();
+
+app.listen (1337, function(){
+    console.log("ready on port 1337");
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+twilio(app);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
